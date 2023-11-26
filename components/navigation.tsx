85373a4a4a7c8ef3497a5706ee50afafdfea0d38
Your name <wedoulilishte@gmail.com>
2023-11-26 11:57:52 +0200

    Add navigation icons

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,8 @@
 'use client'
 //import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
+import theme from "../assets/darkThemeIcon.png";
+import home from "../assets/HomeIcon.png";
 
 
 const Navigation = () => {
@@ -26,20 +28,22 @@ const Navigation = () => {
             styles.setProperty("--background-color", "rgb(20 20 20)");
             styles.setProperty("--box-shadow", "white");
             styles.setProperty("--hover-background-color", "rgb(70 70 70)");
+            styles.setProperty("--icons-color", "0");
         }
         else{
             styles.setProperty("--text-color", "black");
             styles.setProperty("--background-color", "whitesmoke");
             styles.setProperty("--box-shadow", "black");
             styles.setProperty("--hover-background-color", "rgb(200 200 200)");
+            styles.setProperty("--icons-color", "1");
         }
     }
 
     return(
         <>
         {/* <div className="nav-page-transition"></div>  <- Part of the "smooth" transition*/}
-        <Link href={"/"} className="home-image"></Link>
-        <div className="home-image" onClick={changeTheme}></div>
+        <Link href={"/"} className="home-image"><img src={home.src} alt="" className="navigation-images"/></Link>
+        <div className="home-image" onClick={changeTheme}><img src={theme.src} alt="" className="navigation-images"/></div>
         </>
     )
 }
